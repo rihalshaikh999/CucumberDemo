720235482813a0f00bfeb703a47ccdc8087c1618
Rihal Shaikh <rihalshaikh999@gmail.com>
2024-04-01 10:35:53 +0530

    Added cucumber code demo

---
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -0,0 +1,17 @@
+package StepDefinitions;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/Features/GoogleSearch.feature", 
+glue= {"StepDefinitions"},
+monochrome= true,
+plugin = {"pretty", "junit:target/JUnitReports/report.xml",
+		"json:target/JSONReports/report.json",
+		"html:target/HtmlReports"}
+		)
+public class TestRunner {
+
+}
